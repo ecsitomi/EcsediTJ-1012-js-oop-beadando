4ad7e4d4d5004533b3f4d94e5579a306e872038d
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2023-10-12 17:14:43 +0200

    feladat3 konstruktorok

---
+++ feladat3.js
@@ -0,0 +1,9 @@
+class Food{
+    constructor(nev,ar,kategoria,osszetevok){
+        this.nev=nev;
+        this.ar=ar;
+        this.kategoria=kategoria;
+        this.osszetevok=osszetevok;
+    }
+    
+}
