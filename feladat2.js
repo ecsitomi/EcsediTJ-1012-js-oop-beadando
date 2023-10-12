de4dd5e67cd3fa67c64af0c387a1559fa6e2d0a5
Ecsedi Tamás József <ecsi.hgh@gmail.com>
2023-10-12 17:11:04 +0200

    script:scr javítva + feladat 2 befejezve

--- feladat2.js
+++ feladat2.js
@@ -1,5 +1,12 @@
 class Pont {
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+    }
     tavolsagAzOrigotol(){
-        
+        return Math.sqrt(this.x**2+this.y**2);
+    }
+    static tavolsag(p1,p2){
+        return Math.sqrt((p1.x-p2.x)**2+(p1.y-p2.y)**2);
     }
 }
